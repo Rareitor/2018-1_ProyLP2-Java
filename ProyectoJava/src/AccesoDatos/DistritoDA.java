c5a2d1126e2463b4fc8998aa577e8d0bb9e65c1e
Leiden01 <sebasdap97@gmail.com>
2018-05-24 09:25:27 -0500

    mapa

--- ProyectoJava/src/AccesoDatos/DistritoDA.java
+++ ProyectoJava/src/AccesoDatos/DistritoDA.java
@@ -16,14 +16,15 @@ public class DistritoDA {
     public List<String> listarDistritos(){
         List<String> lista = new ArrayList<>();
         try{
-            Class.forName("com.mysql.jb.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection
             ("jdbc:mysql://200.16.7.96/inf282g8", "inf282g8", "4LDJZU");
-            String sql = "{call MOSTRAR_DISTRITOS_ORDEN()";
+            String sql = "{call MOSTRAR_DISTRITOS_ORDEN()}";
             CallableStatement cs = con.prepareCall(sql);
             ResultSet rs = cs.executeQuery();
             while(rs.next()){
                 String distrito = rs.getString("distrito");
+                System.out.println(distrito);
                 lista.add(distrito);
             }
             con.close();
