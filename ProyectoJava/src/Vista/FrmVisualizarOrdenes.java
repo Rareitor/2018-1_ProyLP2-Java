6bb2f3989d364bda38f3650504ec17caafe81a88
Leiden01 <sebasdap97@gmail.com>
2018-05-24 00:21:30 -0500

    VisualizarOrdenes v3

--- ProyectoJava/src/Vista/FrmVisualizarOrdenes.java
+++ ProyectoJava/src/Vista/FrmVisualizarOrdenes.java
@@ -19,46 +19,48 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
         listaOrdenes = new ArrayList<>();
         logicaNegocio = new OrdenBL();
         this.puesto = puesto;
+        
+        
     }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblFechaInicio = new javax.swing.JLabel();
-        lblFechaFin = new javax.swing.JLabel();
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbOrdenes = new javax.swing.JTable();
-        lblTipoReporte = new javax.swing.JLabel();
-        rdoMensual = new javax.swing.JRadioButton();
-        rdoRadio = new javax.swing.JRadioButton();
-        btnBuscar = new javax.swing.JButton();
-        btnImprimir = new javax.swing.JButton();
-        btnExportar = new javax.swing.JButton();
-        btnSalir = new javax.swing.JButton();
-        dateFIni = new com.toedter.calendar.JDateChooser();
-        dateFFin = new com.toedter.calendar.JDateChooser();
         jLabel1 = new javax.swing.JLabel();
         textTotal = new javax.swing.JTextField();
+        btnSalir = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        lblTipoReporte = new javax.swing.JLabel();
+        rdoMensual = new javax.swing.JRadioButton();
+        rdoAnual = new javax.swing.JRadioButton();
+        btnExportar = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        dateFIni = new com.toedter.calendar.JDateChooser();
+        lblFechaInicio = new javax.swing.JLabel();
+        lblFechaFin = new javax.swing.JLabel();
+        dateFFin = new com.toedter.calendar.JDateChooser();
+        btnBuscar = new javax.swing.JButton();
 
+        buttonGroup1.add(rdoMensual);
+        buttonGroup1.add(rdoAnual);
+
+        setTitle("Visualizar Comisiones");
         setToolTipText("Visualizar Órdenes");
-
-        lblFechaInicio.setText("Fecha de Inicio:");
-        lblFechaInicio.setToolTipText("");
-
-        lblFechaFin.setText("Fecha de Fin:");
-        lblFechaFin.setToolTipText("");
 
         tbOrdenes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "Orden", "Comisionista", "Fecha", "Canal", "Monto vendido"
+                "Orden", "idProducto", "Fecha", "Canal", "Monto vendido"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false
+                false, true, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -67,22 +69,10 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
         });
         jScrollPane1.setViewportView(tbOrdenes);
 
-        lblTipoReporte.setText("Seleccione el tipo de reporte:");
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel1.setText("Total: ");
 
-        rdoMensual.setText("Mensual");
-
-        rdoRadio.setText("Anual");
-
-        btnBuscar.setText("Buscar");
-        btnBuscar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnBuscarMouseClicked(evt);
-            }
-        });
-
-        btnImprimir.setText("Imprimir");
-
-        btnExportar.setText("Exportar");
+        textTotal.setEnabled(false);
 
         btnSalir.setText("Salir");
         btnSalir.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -91,10 +81,101 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel1.setText("Total: ");
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Reporte"));
+        jPanel1.setToolTipText("Reporte");
 
-        textTotal.setEnabled(false);
+        lblTipoReporte.setText("Tipo de reporte:");
+
+        rdoMensual.setText("Mensual");
+        rdoMensual.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rdoMensualActionPerformed(evt);
+            }
+        });
+
+        rdoAnual.setText("Anual");
+
+        btnExportar.setText("Exportar");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblTipoReporte)
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(rdoAnual)
+                            .addComponent(rdoMensual)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addComponent(btnExportar)))
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTipoReporte, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(rdoMensual))
+                .addGap(18, 18, 18)
+                .addComponent(rdoAnual)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnExportar)
+                .addGap(22, 22, 22))
+        );
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Filtrar por fecha"));
+
+        lblFechaInicio.setText("Fecha de Inicio:");
+        lblFechaInicio.setToolTipText("");
+
+        lblFechaFin.setText("Fecha de Fin:");
+        lblFechaFin.setToolTipText("");
+
+        btnBuscar.setText("Buscar");
+        btnBuscar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnBuscarMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(28, 28, 28)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lblFechaFin)
+                    .addComponent(lblFechaInicio)
+                    .addComponent(dateFIni, javax.swing.GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+                    .addComponent(dateFFin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 89, Short.MAX_VALUE)
+                .addComponent(btnBuscar)
+                .addGap(40, 40, 40))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(lblFechaInicio)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(9, 9, 9)
+                        .addComponent(dateFIni, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14)
+                        .addComponent(lblFechaFin))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(btnBuscar)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(dateFFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 9, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -104,68 +185,41 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblTipoReporte)
-                        .addGap(47, 47, 47)
-                        .addComponent(rdoMensual)
-                        .addGap(38, 38, 38)
-                        .addComponent(rdoRadio)
-                        .addGap(0, 214, Short.MAX_VALUE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 557, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 724, Short.MAX_VALUE)
+                        .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblFechaInicio)
-                            .addComponent(dateFFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lblFechaFin)
-                                .addGap(145, 145, 145)
-                                .addComponent(btnBuscar))
-                            .addComponent(dateFIni, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(btnImprimir, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnExportar, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnSalir, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(142, 142, 142))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jLabel1)
                         .addGap(18, 18, 18)
-                        .addComponent(textTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
+                        .addComponent(textTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(26, 26, 26)
+                        .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(23, 23, 23))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblTipoReporte)
-                    .addComponent(rdoMensual)
-                    .addComponent(rdoRadio))
-                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblFechaInicio)
-                        .addGap(9, 9, 9)
-                        .addComponent(dateFIni, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblFechaFin)
-                            .addComponent(btnBuscar))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(dateFFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(59, 59, 59)
+                        .addComponent(btnSalir))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnImprimir)
-                        .addGap(30, 30, 30)
-                        .addComponent(btnExportar)
-                        .addGap(30, 30, 30)
-                        .addComponent(btnSalir)))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(textTotal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(97, Short.MAX_VALUE))
+                    .addComponent(textTotal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(34, 34, 34))
         );
 
         pack();
@@ -203,10 +257,10 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
         
         Object[] fila = new Object [3];
         for (int i=0;i<listaOrdenes.size();i++){
-            fila[0] = listaOrdenes.get(i).getIdProducto();
-            fila[1] = listaOrdenes.get(i).getMontoPago();
-            fila[2] = listaOrdenes.get(i).getIdProducto();
-            fila[3] = listaOrdenes.get(i).getIdCanal();
+            fila[0] = listaOrdenes.get(i).getId();
+            fila[1] = listaOrdenes.get(i).getIdProducto();
+            fila[2] = listaOrdenes.get(i).getIdCanal();
+            fila[3] = listaOrdenes.get(i).getFechaVenta();
             fila[4] = listaOrdenes.get(i).getFechaVenta();
             subtotal += listaOrdenes.get(i).getMontoPago();
             modelo.addRow(fila);
@@ -214,22 +268,28 @@ public class FrmVisualizarOrdenes extends javax.swing.JInternalFrame {
         
         textTotal.setText(subtotal.toString());
     }//GEN-LAST:event_btnBuscarMouseClicked
+
+    private void rdoMensualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rdoMensualActionPerformed
+
+    }//GEN-LAST:event_rdoMensualActionPerformed
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnExportar;
-    private javax.swing.JButton btnImprimir;
     private javax.swing.JButton btnSalir;
+    private javax.swing.ButtonGroup buttonGroup1;
     private com.toedter.calendar.JDateChooser dateFFin;
     private com.toedter.calendar.JDateChooser dateFIni;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblFechaFin;
     private javax.swing.JLabel lblFechaInicio;
     private javax.swing.JLabel lblTipoReporte;
+    private javax.swing.JRadioButton rdoAnual;
     private javax.swing.JRadioButton rdoMensual;
-    private javax.swing.JRadioButton rdoRadio;
     private javax.swing.JTable tbOrdenes;
     private javax.swing.JTextField textTotal;
     // End of variables declaration//GEN-END:variables
